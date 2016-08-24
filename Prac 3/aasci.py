d1d03ece12c30aa1f37d27c59ef8356586bed847
alexvonkurtz <alexander.vonkurtz@gmail.com>
2016-08-25 01:35:07 +1000

    Add function for getting number and error checking

---
+++ Prac 3/aasci.py
@@ -0,0 +1,22 @@
+def main ():
+    LOWER = 33
+    UPPER = 127
+    character = input("Enter a character: ")
+    print("The ASCII code of '" + character + "' is",ord(character))
+    number = get_number(LOWER, UPPER)
+    print("The character of '" + str(number) + "' is", chr(number))
+    for ascii in range(33, 127):
+        print(ascii, '{:>10}'.format(chr(ascii)))
+
+def get_number(LOWER,UPPER):
+    while True:
+        try:
+            number = int(input("Enter a number between 33 and 127: "))
+        except ValueError:
+            print ("Please enter a valid number!")
+            continue
+        if number < LOWER or number > UPPER:
+            print ("Please enter a valid number!")
+        else:
+            return number
+main()
