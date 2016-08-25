8af5cea1848b955717b680f30a25c5a7e25ccc45
alexander.vonkurtz <Bobminni1>
2016-08-25 10:06:13 +1000

    Add functions with user increment input

---
+++ Prac 2/userName.py
@@ -0,0 +1,24 @@
+#ask user for name
+# error checking to make sure username not blank
+# print every second letter in name
+# use for loop, range and length of the name
+
+def main():
+    userName = get_name()
+    method_name(userName)
+
+
+def method_name(userName):
+    increment = int(input("Enter increment number: "))
+    print(userName[1::increment])
+
+
+def get_name():
+    userName = input("Please enter your name: ")
+    while userName == "":
+        print("You did not enter anything")
+        userName = input("Please enter your name: ")
+    return userName
+
+
+main()
